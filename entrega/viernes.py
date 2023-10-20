a17d11829b3c3536c3543bafa4e051fa1c551e97
TeooProgrammer <141300412+TeooProgrammer@users.noreply.github.com>
2023-10-20 21:56:24 +0000

    uwu

---
+++ entrega/viernes.py
@@ -0,0 +1,8 @@
+print("Viernes en AIpython")
+print("Las maquinas me sorprenden con mucha frecuencia")
+print()
+print('"23" 23')
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciendole creer que es un humano.')
+
+
+
